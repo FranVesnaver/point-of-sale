dd872142cac3731b2895db3a15b6be68e0faaf03
FranVesnaver <fevesnaver@gmail.com>
2026-03-03 22:37:11 -0300

    add equals and hashcode to product entity

--- backend/src/main/java/com/superpos/model/Product.java
+++ backend/src/main/java/com/superpos/model/Product.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,6 +20,7 @@ import java.math.BigDecimal;
 )
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Product {
 
     @Id
