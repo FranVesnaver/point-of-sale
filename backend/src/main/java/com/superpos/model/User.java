25d17146698322c6c466c8056b6dbb3502caf972
FranVesnaver <fevesnaver@gmail.com>
2026-03-23 18:14:25 -0300

    add a user entity

---
+++ backend/src/main/java/com/superpos/model/User.java
@@ -0,0 +1,31 @@
+package com.superpos.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    @Setter
+    private String username;
+
+    @NotBlank
+    @Column(nullable = false)
+    @Setter
+    private String password;
+
+    @Column(name = "is_admin", nullable = false)
+    @Setter
+    private boolean isAdmin;
+
+}
