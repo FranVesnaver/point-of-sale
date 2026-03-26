8255ece9224b0720c5fa4faa760c13e307991039
FranVesnaver <fevesnaver@gmail.com>
2026-03-26 18:56:56 -0300

    add authentication logic in user service

--- backend/src/main/java/com/superpos/service/UserService.java
+++ backend/src/main/java/com/superpos/service/UserService.java
@@ -1,6 +1,7 @@
 package com.superpos.service;
 
 import com.superpos.exception.ExistingUsernameException;
+import com.superpos.exception.InvalidCredentialsException;
 import com.superpos.model.User;
 import com.superpos.repository.UserRepository;
 import org.springframework.stereotype.Service;
@@ -9,9 +10,11 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordHasher passwordHasher;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordHasher passwordHasher) {
         this.userRepository = userRepository;
+        this.passwordHasher = passwordHasher;
     }
 
     public User createUser(String username, String password, boolean admin) {
@@ -19,9 +22,30 @@ public class UserService {
 
         User user = new User();
         user.setUsername(username);
-        user.setPassword(password);
+        user.setPassword(passwordHasher.hash(password));
         user.setAdmin(admin);
 
         return userRepository.save(user);
     }
+
+    public User authenticate(String username, String password) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(InvalidCredentialsException::new);
+
+        if (!passwordHasher.matches(password, user.getPassword())) {
+            throw new InvalidCredentialsException();
+        }
+
+        if (!passwordHasher.isHashed(user.getPassword())) {
+            user.setPassword(passwordHasher.hash(password));
+            return userRepository.save(user);
+        }
+
+        return user;
+    }
+
+    public boolean hasUsers() {
+        return userRepository.count() > 0;
+    }
+
 }
