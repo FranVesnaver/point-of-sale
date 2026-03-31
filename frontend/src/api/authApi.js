4ed5b7f9a28b5974508b7dcea78110a9a7026804
FranVesnaver <fevesnaver@gmail.com>
2026-03-31 20:15:54 -0300

    add frontend endpoint to logout

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,12 +1,19 @@
 import { request } from "./apiUtils.js";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "/api";
-const API_URL = `${API_BASE_URL}/auth/login`;
+const LOGIN_API_URL = `${API_BASE_URL}/auth/login`;
+const LOGOUT_API_URL = `${API_BASE_URL}/auth/logout`;
 
 export async function login(username, password) {
-    return request(API_URL, {
+    return request(LOGIN_API_URL, {
         method: "POST",
         headers: { "content-type": "application/json" },
         body: JSON.stringify({ username, password }),
     }, "Failed to login");
+}
+
+export async function logout() {
+    return request(LOGOUT_API_URL, {
+        method: "POST",
+    }, "Failed to logout");
 }
