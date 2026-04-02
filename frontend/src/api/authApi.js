b1273f1e3f7d86549fd68f86480eddea9da3b87d
FranVesnaver <fevesnaver@gmail.com>
2026-04-02 19:33:48 -0300

    add front endpoint to check if there are admins in the system

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,6 +3,7 @@ import { request } from "./apiUtils.js";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "/api";
 const LOGIN_API_URL = `${API_BASE_URL}/auth/login`;
 const LOGOUT_API_URL = `${API_BASE_URL}/auth/logout`;
+const BOOTSTRAP_API_URL = `${API_BASE_URL}/auth/bootstrap`;
 
 export async function login(username, password) {
     return request(LOGIN_API_URL, {
@@ -16,4 +17,10 @@ export async function logout() {
     return request(LOGOUT_API_URL, {
         method: "POST",
     }, "Failed to logout");
+}
+
+export async function bootstrap() {
+    return request(BOOTSTRAP_API_URL, {
+        method: "GET",
+    }, "Failed to get bootstrap status");
 }
